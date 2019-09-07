2669f7165b6bc3fcc53795e846f61da40717e6c8
The-Ben <ben@theaarons.com>
2019-09-07 16:14:57 -0700

    Add limit arg to constructor

--- bda_tools/generators.py
+++ bda_tools/generators.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Any, Union
+from typing import Iterable, Any
 from inspect import isgenerator
 from itertools import islice
 
@@ -6,16 +6,18 @@ class Caching_Generator(object):
     """Wraps a generator and caches returned objects.
     The cache can be iterated over multiple times, unlike a standard generator.
     """
-    def __init__(self, gen: Iterable, limit: Union[int, None] = None) -> None:
+    def __init__(self, gen: Iterable, limit: int=0) -> None:
         self.generator: Iterable[Any] = gen
         self.cache: List[Any] = []
-        self.limit: Union[int, None] = limit
+        self.limit: int = limit
 
     def __iter__(self) -> Iterable:
         self._position = 0
         return self
     
     def __next__(self) -> Any:
+        if len(self) >= self.limit and self.limit:
+            raise StopIteration
         if len(self.cache) > self._position:
             item = self.cache[self._position]
         else:
